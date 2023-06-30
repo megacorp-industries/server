b99a133d8d395f25a06d6e08b0af4ad0a8b495ab
some guy <someguy@somedomain.com>
2023-06-30 21:55:54 +0930

    Added libvirt dir and create-coreos.sh script

---
+++ libvirt/create-coreos.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+IGN_CONFIG=/var/KVM/config.ign
+IMAGE=/var/lib/libvirt/images/coreos38.qcow2
+VM_NAME=node$1
+VCPUS=2
+RAM_MB=4096
+DISK_GB=30
+STREAM=stable
+
+virt-install --connect="qemu:///system" --name="${VM_NAME}" \
+    --vcpus="${VCPUS}" --memory="${RAM_MB}" \
+    --os-variant="fedora-coreos-$STREAM" --import --graphics=none \
+    --disk="size=${DISK_GB},backing_store=${IMAGE}" \
+    --qemu-commandline="-fw_cfg name=opt/com.coreos/config,file=${IGN_CONFIG}"
