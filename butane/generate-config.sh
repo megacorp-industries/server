ef8851fb9541cd3c8e4b3dcd73efc88ead85b23c
some guy <someguy@somedomain.com>
2023-09-06 21:29:03 +0930

    lazy bash script for producing ign config

--- butane/generate-config.sh
+++ butane/generate-config.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+#
+# Generate butane config
+podman run --interactive --rm --security-opt label=disable \
+       --volume ${PWD}:/pwd --workdir /pwd quay.io/coreos/butane:release \
+       --pretty --strict config.bu > /var/lib/libvirt/images/config.ign
 
-butane config.bu > /var/lib/libvirt/images/config.ign
+# Fix permissions on ignition file
+chown root:root /var/lib/libvirt/images/config.ign
