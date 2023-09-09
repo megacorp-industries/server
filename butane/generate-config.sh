30158a057f4684003c39110ddbd753a58c04156b
some guy <someguy@somedomain.com>
2023-09-09 10:59:51 +0930

    fixed broken ignition generation script

--- butane/generate-config.sh
+++ butane/generate-config.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-#
+
 # Generate butane config
 podman run --interactive --rm --security-opt label=disable \
        --volume ${PWD}:/pwd --workdir /pwd quay.io/coreos/butane:release \
-       --pretty --strict config.bu > /var/lib/libvirt/images/config.ign
+       --pretty --strict config.bu > config.ign
 
 # Fix permissions on ignition file
-chown root:root /var/lib/libvirt/images/config.ign
+chown root:root config.ign
+
+#  Move generated ignition config to libvirt images dir
+mv config.ign /var/lib/libvirt/images
